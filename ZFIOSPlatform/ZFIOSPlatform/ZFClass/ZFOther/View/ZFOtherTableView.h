eb97f35ac37f0b543240b9203f930fcf995b09a7
ZMF <zhangmufeng@outlook.com>
2017-05-22 11:22:53 +0800

    小处理

---
+++ ZFIOSPlatform/ZFIOSPlatform/ZFClass/ZFOther/View/ZFOtherTableView.h
@@ -0,0 +1,13 @@
+//
+//  ZFOtherTableView.h
+//  ZFIOSPlatform
+//
+//  Created by 张木锋 on 2017/5/18.
+//  Copyright © 2017年 COM.ZMF. All rights reserved.
+//
+
+#import "ZFBaseTableView.h"
+
+@interface ZFOtherTableView : ZFBaseTableView
+
+@end
---
+++ ZFIOSPlatform/ZFIOSPlatform/ZFClass/ZFOther/View/ZFOtherTableView.m
@@ -0,0 +1,14 @@
+//
+//  ZFOtherTableView.m
+//  ZFIOSPlatform
+//
+//  Created by 张木锋 on 2017/5/18.
+//  Copyright © 2017年 COM.ZMF. All rights reserved.
+//
+
+#import "ZFOtherTableView.h"
+
+@implementation ZFOtherTableView
+
+
+@end
